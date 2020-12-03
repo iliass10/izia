70f3f20409f68b318194581a2a1b55c49b17b7e3
bruno fialho <fialho.bruno@live.fr>
2020-12-03 10:52:54 +0100

    seeds

--- db/migrate/20201202105146_add_fisrt_name_to_user.rb
+++ db/migrate/20201202105146_add_fisrt_name_to_user.rb
@@ -2,9 +2,6 @@ class AddFisrtNameToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :gender, :string
-    add_column :users, :birthdate :t.date
-    add_column :users, :weight :interger
-    add_column :users, :height :integer
+
   end
 end
