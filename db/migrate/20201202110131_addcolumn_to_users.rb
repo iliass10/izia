a1b3761251fdc1976d9edfa2aff2c661f9b385ef
Bruno <70034377+brunodfialho@users.noreply.github.com>
2020-12-03 10:56:18 +0100

    Delete 20201202110131_addcolumn_to_users.rb

--- db/migrate/20201202110131_addcolumn_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddcolumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :gender, :string
-    add_column :users, :birthdate, :date
-    add_column :users, :weight, :integer
-    add_column :users, :height, :integer
-  end
-end
