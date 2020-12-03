f29c0f91144878ed69a103a676161c23a7cecf19
bruno fialho <fialho.bruno@live.fr>
2020-12-03 11:10:24 +0100

    création des seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Result.destroy_all
+Appointment.destroy_all
+Metric.destroy_all
+Test.destroy_all
+User.destroy_all
+puts "------> Create users"
+thomas = User.create!(email: "thomas@lewagon.org", password: "123456", password_confirmation: "123456")
+adem = User.create!(email: "adem@lewagon.org", password: "123456", password_confirmation: "123456")
+illias = User.create!(email: "illiass@lewagon.org", password: "123456", password_confirmation: "123456")
+bruno = User.create!(email: "bruno@lewagon.org", password: "123456", password_confirmation: "123456")
+puts "------> Create tests"
+blood = Test.create!(name: "Test sanguin", description: "Soyez créatif")
+puts "------> Define metrics"
+hb = Metric.create!(name: "Taux de hemoglobine", test: blood)
+calcium = Metric.create!(name: "Taux de calcium", test: blood)
+iron = Metric.create!(name: "Taux de fer", test: blood)
+puts "------> Create some appointments... "
+a1 = Appointment.create!(user: thomas, test:blood, datetime: Time.zone.now)
+a2 = Appointment.create!(user: adem, test:blood, datetime: Time.zone.yesterday)
+a3 = Appointment.create!(user: illias, test:blood, datetime: Time.zone.now)
+puts "------> Let's set the results "
+Result.create(appointment:a1, metric: hb, value: 98)
+Result.create(appointment:a1, metric: iron, value: 130)
+Result.create(appointment:a1, metric: calcium, value: 40)
+Result.create(appointment:a2, metric: hb, value: 98)
+Result.create(appointment:a2, metric: iron, value: 130)
+Result.create(appointment:a2, metric: calcium, value: 40)
+Result.create(appointment:a3, metric: hb, value: 98)
+Result.create(appointment:a3, metric: iron, value: 130)
+Result.create(appointment:a3, metric: calcium, value: 40)
